cb1548a0ef91e237877e500951647f44f39a9877
Alexandre de Souza <alexandre@aledsz.com.br>
2021-10-26 00:33:01 -0300

    add ecto test case

--- .formatter.exs
+++ .formatter.exs
@@ -2,5 +2,5 @@
 [
   import_deps: [:ecto],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [field: 3]
+  locals_without_parens: [field: 3, assert_valid_changeset: 1, refute_valid_changeset: 1]
 ]
